decc8d24d8e14ec1ebf2a9844b5b761cde6ee39c
WYseven <consolelog@126.com>
2017-03-21 22:07:58 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view></router-view> -->
+
+    <div class="main">
+      <input type="" name="" />
+      <div class="content">
+        闲鱼主页
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.main {
+  border: 1px solid #000;
+  height: 1000px;
+}
+
+input{
+  width: 80%;
+  height: 30px;
+  margin-top:10px;
+}
+
+.content {
+  font-size: 50px;
+}
+
+</style>
